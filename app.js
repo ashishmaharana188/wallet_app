bd47432886704b97631f2db289224660ea2ae807
Ashish Kumar Maharana <86043589+ashishmaharana188@users.noreply.github.com>
2024-08-14 15:14:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// query selector
+const cardTypeSelector = document.querySelector(".card_type_text");
+const accountNumberSelector = document.querySelector(".num_text");
+
+// global variables for intro page
+let cardType = "";
+let accountNumber = "";
+
+function go_app() {}
